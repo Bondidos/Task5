8d02b697e24a862592980d1f7143e8a81e642bd1
Bondidos <gglbond@gmail.com>
2021-09-27 01:34:35 +0300

    feature: save Image

--- app/src/main/java/com/bondidos/task5/utils/SaveImage.kt
+++ app/src/main/java/com/bondidos/task5/utils/SaveImage.kt
@@ -1,36 +1,51 @@
 package com.bondidos.task5.utils
 
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
-import androidx.core.content.res.TypedArrayUtils.getString
-import com.bondidos.task5.R
+import androidx.annotation.RequiresApi
 import com.bondidos.task5.adapter.cat_holder.Cat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
-import java.io.File
-import java.io.FileOutputStream
+import java.io.IOException
 
 
+@RequiresApi(Build.VERSION_CODES.Q)////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 fun Cat.downloadAndSave(context: Context){
-
-    val dirPath = Environment.getExternalStorageDirectory().absolutePath + "/" + Environment.DIRECTORY_DOWNLOADS
-    val dir = File(dirPath)
     //todo dialog for save name
+    //path to directory with saved images
+    val dirPath = Environment.DIRECTORY_DCIM + "/savedCats"
+    //Mime type of the content
+    val mime = "image/*"
+    // name of the saved image
     val fileName = this.picture.substring(this.picture.lastIndexOf('/') +1)
+    // describe image
+    val values = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+        put(MediaStore.MediaColumns.MIME_TYPE, mime)
+        put(MediaStore.MediaColumns.RELATIVE_PATH, dirPath)
+    }
+    //provides app. access to content model
+    val resolver = context.contentResolver
 
     Glide.with(context)
         .load(this.picture)
+            //save image to the store
         .into(object: CustomTarget<Drawable>(){
             override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
                 val bitmap = ((resource as BitmapDrawable).bitmap)
                 Toast.makeText(context, "Saving Image...", Toast.LENGTH_SHORT).show()
-                saveImage(bitmap, dir,fileName,context)
+                saveImage(context,resolver,values,bitmap)
             }
 
             override fun onLoadCleared(placeholder: Drawable?) {
@@ -43,23 +58,25 @@ fun Cat.downloadAndSave(context: Context){
             }
         })
 }
-private fun saveImage(image: Bitmap,storageDir: File,imageFileName: String, context: Context){
 
-    var successDirCreated = false
-    if (!storageDir.exists()) {
-        successDirCreated = storageDir.mkdir()
+private fun saveImage(context: Context, resolver: ContentResolver, values: ContentValues, bitmap: Bitmap){
+    var uri: Uri? = null
+    try{
+         uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            ?: throw IOException("failed to create new MediaStore record.")
+
+        resolver.openOutputStream(uri)?.use {
+            if (!bitmap.compress(Bitmap.CompressFormat.JPEG,100, it)) {
+                Toast.makeText(context, "Failed to save Image...", Toast.LENGTH_SHORT).show()
+                throw IOException("Failed to save bitmap.")
+            }
+            Toast.makeText(context, "Image saved.", Toast.LENGTH_SHORT).show()
+        } ?: throw IOException("Failed to open output stream.")
+    } catch (e: IOException){
+        uri?.let {
+            Uri ->
+            resolver.delete(Uri,null,null)
+        }
+        throw e
     }
-
-    val imageFile = File(storageDir,imageFileName)
-    val savedImagePath = imageFile.absolutePath
-    //try {
-        //open outStream and write image into file
-        val fOut = FileOutputStream(imageFile)
-        image.compress(Bitmap.CompressFormat.JPEG,100,fOut)
-        fOut.close()
-        Toast.makeText(context, "Image saved!", Toast.LENGTH_SHORT).show()
-   /* } catch (e: Exception){
-        Toast.makeText(context, "Error while saving Image", Toast.LENGTH_SHORT).show()
-        e.stackTrace
-    }*/
 }
